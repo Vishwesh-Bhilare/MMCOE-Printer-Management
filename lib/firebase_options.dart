03b6002da41415b8ef0ae2247dd9f71c2be9d5ea
Vishwesh Bhilare <bhilarevishwesh@gmail.com>
2025-10-05 15:33:15 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -41,7 +41,7 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyC4XrdLKBPplcPX5IfFrcSTAZ1CuGryMb0',
+    apiKey: 'YOUR_API_KEY',
     appId: '1:38499636597:web:29ff2b8d2a3ee8b0e02f3c',
     messagingSenderId: '38499636597',
     projectId: 'print-management-app',
@@ -51,7 +51,7 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAFc7oVrlnlca-rGR6cyx0cvk-LQUifmz8',
+    apiKey: 'YOUR_API_KEY',
     appId: '1:38499636597:android:fcd689c0f1425022e02f3c',
     messagingSenderId: '38499636597',
     projectId: 'print-management-app',
@@ -59,7 +59,7 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCVpgX6oMQnlPywHyDWFFDBL8V5Jjw-Wlk',
+    apiKey: 'YOUR_API_KEY',
     appId: '1:38499636597:ios:67b4c1e00cfb8a61e02f3c',
     messagingSenderId: '38499636597',
     projectId: 'print-management-app',
@@ -68,7 +68,7 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyCVpgX6oMQnlPywHyDWFFDBL8V5Jjw-Wlk',
+    apiKey: 'YOUR_API_KEY',
     appId: '1:38499636597:ios:67b4c1e00cfb8a61e02f3c',
     messagingSenderId: '38499636597',
     projectId: 'print-management-app',
@@ -77,7 +77,7 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyC4XrdLKBPplcPX5IfFrcSTAZ1CuGryMb0',
+    apiKey: 'YOUR_API_KEY',
     appId: '1:38499636597:web:a5266966f6d826e1e02f3c',
     messagingSenderId: '38499636597',
     projectId: 'print-management-app',
